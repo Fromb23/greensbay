d1728e619f18750f679dd430ff1cdc9a99f35d6f
Fromb23 <rombo.programming@gmail.com>
2025-03-05 14:57:47 +0300

    editing admin products

--- frontend/src/components/Products.jsx
+++ frontend/src/components/Products.jsx
@@ -5,16 +5,36 @@ import { FaTrash, FaEdit, FaPlus, FaFilter } from "react-icons/fa";
 
 const Products = () => {
   const [editingRow, setEditingRow] = useState(null);
-  const [editedData, setEditedData] = useState(null);
+  const [editingCell, setEditingCell] = useState(null);
+  const [editedData, setEditedData] = useState({});
   const inputRef = useRef(null); 
   const [products, setProducts] = useState([]);
   const [selectedProducts, setSelectedProducts] = useState([]);
   const [showCreateForm, setShowCreateForm] = useState(false);
   const [filter, setFilter] = useState("All");
+  const categories = [
+    { id: 1, name: "Electronics" },
+    { id: 2, name: "Clothing" },
+    { id: 3, name: "Home & Kitchen" },
+  ];
 
-  const handleRowSelect = (id) => {
-    setEditingRow(id);
-    setEditedData(products.find((product) => product.id === id));
+  const handleEdit = (productId, field, value) => {
+    setEditedData((prev) => ({
+      ...prev,
+      [productId]: {
+        ...prev[productId],
+        [field]: field === "category" ? categories.find((c) => c.id == value) : value,
+      },
+    }));
+  };  
+
+  const handleBlur = (productId) =>{
+    setProducts((prevProducts) =>
+      prevProducts.map((product) =>
+        product.id === productId ? { ...product, ...editedData[productId] } : product
+      )
+    );
+    setEditingCell(null);
   };
 
   useEffect(() => {
@@ -148,14 +168,12 @@ const Products = () => {
                   </td>
                   <td className="py-2 px-4 border">{product.id}</td>
                   <td className="py-2 px-4 border">
-                    {editingRow === product.id ? (
+                    {editingCell === `${product.id}-image` ? (
                       <input
-                        ref={inputRef}
                         type="text"
-                        value={editedData?.image || ""}
-                        onChange={(e) =>
-                          setEditedData({ ...editedData, image: e.target.value })
-                          }
+                        value={editedData[product.id]?.image ?? product.image}
+                        onChange={(e) => handleEdit(product.id, "image", e.target.value)}
+                        onBlur={() => handleBlur(product.id)}
                         className="border rounded px-2 py-1"
                         autoFocus
                       />
@@ -168,16 +186,93 @@ const Products = () => {
                           }
                           alt={product.name}
                           className="w-10 h-10 object-cover rounded"
-                          onDoubleClick={() => handleRowSelect(product.id)}
+                          onDoubleClick={() => setEditingCell(`${product.id}-image`)}
                         />
                         )
                     }
                   </td>
-                  <td className="py-2 px-4 border">{product.name}</td>
-                  <td className="py-2 px-4 border">{product.units_left}</td>
-                  <td className="py-2 px-4 border">${product.discount_price}</td>
-                  <td className="py-2 px-4 border">${product.actual_price}</td>
-                  <td className="py-2 px-4 border">{product.category?.name}</td>
+                  <td className="py-2 px-4 border">
+                    {editingCell === `${product.id}-name` ? (
+                      <input
+                        type="text"
+                        value={editedData[product.id]?.name ?? product.name}
+                        onChange={(e) => handleEdit(product.id, "name", e.target.value)}
+                        onBlur={() => handleBlur(product.id)}
+                        className="border rounded px-2 py-1"
+                        autoFocus
+                        />
+                      ) : (
+                        <span onDoubleClick={() => setEditingCell(`${product.id}-name` )}>{product.name}</span>
+                        )
+                      }
+                  </td>
+                  <td className="py-2 px-4 border">
+                    {editingCell === `${product.id}-units_left` ? (
+                      <input
+                        type="number"
+                        value={editedData[product.id]?.units_left ?? product.units_left}
+                        onChange={(e) => handleEdit(product.id, "units_left", e.target.value)}
+                        onBlur={() => handleBlur(product.id)}
+                        className="border rounded px-2 py-1"
+                        autoFocus
+                        />
+                      ) : (
+                        <span onDoubleClick={() => setEditingCell(`${product.id}-units_left`)}>{product.units_left}</span>
+                        )
+                      }
+                  </td>
+                  <td className="py-2 px-4 border">
+                    {editingCell === `${product.id}-discount_price` ? (
+                      <input
+                        type="number"
+                        value={editedData[product.id]?.discount_price ?? product.discount_price}
+                        onChange={(e) => handleEdit(product.id, "discount_price", e.target.value)}
+                        onBlur={() => handleBlur(product.id)}
+                        className="border rounded px-2 py-1"
+                        autoFocus
+                        />
+                      ) : (
+                        <span onDoubleClick={() => setEditingCell(`${product.id}-discount_price`)}>{product.discount_price}</span>
+                        )
+                      }
+                  </td>
+                  <td className="py-2 px-4 border">
+                    {editingCell === `${product.id}-actual_price` ? (
+                      <input
+                        type="number"
+                        value={editedData[product.id]?.actual_price ?? product.actual_price}
+                        onChange={(e) => handleEdit(product.id, "actual_price", e.target.value)}
+                        onBlur={() => handleBlur(product.id)}
+                        className="border rounded px-2 py-1"
+                        autoFocus
+                        />
+                      ) : (
+                        <span onDoubleClick={() => setEditingCell(`${product.id}-actual_price`)}>{product.actual_price}</span>
+                        )
+                      }
+                  </td>
+                  <td className="py-2 px-4 border">
+                    {editingCell === `${product.id}-category` ? (
+                      <select
+                        value={editedData[product.id]?.category ?? product.category?.id}
+                        onChange={(e) => handleEdit(product.id, "category", e.target.value)}
+                        onBlur={() => handleBlur(product.id)}
+                        className="border rounded px-2 py-1"
+                        autoFocus
+                      >
+                    {categories.map((cat) => (
+                      <option key={cat.id} value={cat.id}>
+                        {cat.name}
+                      </option>
+                      ))}
+                      </select>
+                      ) : (
+                        <span onDoubleClick={() => setEditingCell(`${product.id}-category`)}>
+                          {product.category?.name}
+                        </span>
+                    )}
+                  </td>
+
                   <td className="py-6 px-4 flex items-center border-b border-r justify-center gap-2">
                     <button className="text-blue-500 hover:underline flex items-center gap-1">
                       <FaEdit />
